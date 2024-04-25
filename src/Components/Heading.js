cf5ec33f7633714842f7cf10057166f4a00c6693
nico <nico_torregrosa@hotmail.com>
2024-04-25 19:21:37 -0300

    Creating components

---
+++ src/Components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            <h1 className='display-4 text-center'>Lista de tareas</h1>
+            <hr />
+        </div>
+    );
+};
+
+export default Heading;
---
+++ src/Components/SubHeading.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SubHeading extends Component {
+  render() {
+    return (
+        <p className='text-center lead'>Lista de tareas usando React</p>
+    )
+  }
+}
